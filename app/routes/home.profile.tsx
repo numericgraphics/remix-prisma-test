acaca8df035e2bd0f30051a08d3a58769da786ac
rinaldo stevenazzi <rinaldostevenazzi@rinaldos-macbook-small.home>
2023-08-31 17:44:33 +0200

    WIP - add delete account logic

--- app/routes/home.profile.tsx
+++ app/routes/home.profile.tsx
@@ -3,12 +3,12 @@ import type { LoaderFunction, ActionFunction } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { Modal } from "~/components/modal";
-import { getUser, requireUserId } from "~/utils/auth.server";
+import { getUser, requireUserId, logout } from "~/utils/auth.server";
 import { FormField } from "~/components/form-field";
 import { departments } from "~/utils/constants";
 import { SelectBox } from "~/components/select-box";
 import { validateName } from "~/utils/validators.server";
-import { updateUser } from "~/utils/user.server";
+import { updateUser, deleteUser } from "~/utils/user.server";
 import type { Department } from "@prisma/client";
 import { ImageUploader } from "~/components/image-uploader";
 
@@ -18,34 +18,44 @@ export const action: ActionFunction = async ({ request }) => {
   let firstName = form.get("firstName");
   let lastName = form.get("lastName");
   let department = form.get("department");
+  const action = form.get("_action");
 
-  if (
-    typeof firstName !== "string" ||
-    typeof lastName !== "string" ||
-    typeof department !== "string"
-  ) {
-    return json({ error: `Invalid Form Data` }, { status: 400 });
+  switch (action) {
+    case "save":
+      if (
+        typeof firstName !== "string" ||
+        typeof lastName !== "string" ||
+        typeof department !== "string"
+      ) {
+        return json({ error: `Invalid Form Data` }, { status: 400 });
+      }
+
+      const errors = {
+        firstName: validateName(firstName),
+        lastName: validateName(lastName),
+        department: validateName(department),
+      };
+
+      if (Object.values(errors).some(Boolean))
+        return json(
+          { errors, fields: { department, firstName, lastName } },
+          { status: 400 }
+        );
+
+      await updateUser(userId, {
+        firstName,
+        lastName,
+        department: department as Department,
+      });
+
+      return redirect("/home");
+    case "delete":
+      await deleteUser(userId);
+      return logout(request);
+      break;
+    default:
+      return json({ error: `Invalid Form Data` }, { status: 400 });
   }
-
-  const errors = {
-    firstName: validateName(firstName),
-    lastName: validateName(lastName),
-    department: validateName(department),
-  };
-
-  if (Object.values(errors).some(Boolean))
-    return json(
-      { errors, fields: { department, firstName, lastName } },
-      { status: 400 }
-    );
-
-  await updateUser(userId, {
-    firstName,
-    lastName,
-    department: department as Department,
-  });
-
-  return redirect("/home");
 };
 
 export const loader: LoaderFunction = async ({ request }) => {
@@ -97,7 +107,12 @@ export default function ProfileSettings() {
             />
           </div>
           <div className="flex-1">
-            <form method="post">
+            <form
+              method="post"
+              onSubmit={(e) =>
+                !confirm("Are you sure?") ? e.preventDefault() : true
+              }
+            >
               <FormField
                 htmlFor="firstName"
                 label="First Name"
@@ -120,8 +135,20 @@ export default function ProfileSettings() {
                 onChange={(e) => handleInputChange(e, "department")}
               />
 
+              <button
+                name="_action"
+                value="delete"
+                className="rounded-xl w-full bg-red-300 font-semibold text-white mt-4 px-16 py-2 transition duration-300 ease-in-out hover:bg-red-400 hover:-translate-y-1"
+              >
+                Delete Account
+              </button>
+
               <div className="w-full text-right mt-4">
-                <button className="rounded-xl bg-yellow-300 font-semibold text-blue-600 px-16 py-2 transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1">
+                <button
+                  className="rounded-xl bg-yellow-300 font-semibold text-blue-600 px-16 py-2 transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1"
+                  name="_action"
+                  value="save"
+                >
                   Save
                 </button>
               </div>
